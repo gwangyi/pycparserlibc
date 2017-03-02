b2d9119af0284c72b75524f656c145e1b4a3d686
Sungkwang Lee <gwangyi.kr@gmail.com>
2017-03-02 14:11:15 +0900

    setup.py added

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="pycparserlibc",
+    description="pycparser extension that includes fake_libc_include",
+    license='MIT',
+    version='0.1',
+    author='Sungkwang Lee',
+    maintainer='Sungkwang Lee',
+    author_email='gwangyi.kr@gmail.com',
+    url='https://github.com/gwangyi/pycparserlibc',
+    platform='Cross Platform',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',],
+    install_requires=['pycparser>=2.17'],
+    packages=['pycparserlibc'],
+)
